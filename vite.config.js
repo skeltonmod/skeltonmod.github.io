6a4b724637318ae327970b9c43d9fbcc1f2101f8
skeltonmod <spacecat34@gmail.com>
2024-11-03 01:50:47 +0800

    some changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,17 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // Since you're deploying to skeltonmod.github.io
   plugins: [svelte()],
-  base: "./"
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash][extname]'
+      }
+    }
+  }
 })
