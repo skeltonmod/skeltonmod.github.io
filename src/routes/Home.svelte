9c0c38cf1421ce2c5710d57ce0e40802851c3ac6
Elijah <elijah.abgao@fligno.com>
2024-11-09 11:11:22 +0800

    Add some time

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -22,6 +22,10 @@
         month: "2-digit",
         day: "2-digit",
         year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false
       });
     }
   });
