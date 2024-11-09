ad2a514dd12b95df973360176211da05fd6e7d86
Elijah <elijah.abgao@fligno.com>
2024-11-09 11:10:41 +0800

    Fix some things

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,4 +1,31 @@
-<script></script>
+<script>
+  import { onMount } from "svelte";
+  import { fetchEvents } from "../methods/git";
+
+  let current_date = new Date().toLocaleDateString("en-US", {
+    month: "2-digit",
+    day: "2-digit",
+    year: "numeric",
+  });
+
+  onMount(async () => {
+    const data = await fetchEvents();
+
+    const recentPortfolioEvent = data.filter((item) => {
+      return item.repo.id == 410241753;
+    });
+
+    if (recentPortfolioEvent) {
+      current_date = new Date(
+        recentPortfolioEvent[0].created_at
+      ).toLocaleDateString("en-US", {
+        month: "2-digit",
+        day: "2-digit",
+        year: "numeric",
+      });
+    }
+  });
+</script>
 
 <h4 class="home-header" style="font-size: 2rem !important">
   <strong>Skeltonmod</strong>
@@ -14,4 +41,5 @@
     <strong>Unorthodox developer with a lot of free time</strong>
   </p>
   <p>You can also find my work related stuff here</p>
+  <p>Site was last updated at <strong>{current_date}</strong></p>
 </div>
