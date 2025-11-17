e05605ba62393bc602b227b77db60001c37d3ab2
novidash <spacecat34@gmail.com>
2025-11-17 21:16:58 +0800

    new portfolio: minimal

--- src/routes/NotFound.svelte
+++
@@ -1,9 +0,0 @@
-<script></script>
-
-<h4 class="home-header" style="font-size: 2rem !important">
-    404
-</h4>
-<div class="avatar-container">
-    <img src="/smoke.gif" class="avatar" alt="Cool skeleton gif from the 80s or something" />
-    <p>Uhh ohh, the page you are looking for does not exist</p>
-</div>
